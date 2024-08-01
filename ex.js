749ca7dd0e7b4502dd9de0cec8f0772b30b08957
kimdhs <149690629+kimdhs@users.noreply.github.com>
2024-08-01 13:21:45 +0900

    Create ex.js
    Signed-off-by: kimdhs <149690629+kimdhs@users.noreply.github.com>

---
+++ ex.js
@@ -0,0 +1,13 @@
+<html>
+<body onload=exploit()>
+</body> 
+<script>
+        function sleep(ms) {
+                  return new Promise((r) => setTimeout(r, ms));
+        }
+        async function exploit(){
+                url1 = 'https://sbzklyp.request.dreamhack.games';
+                window.open(url1);
+        }
+</script>
+</html>
